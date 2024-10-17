d724d7c965a95f7a3ea3b4dc00a23bff1091c3de
Alexandre Vautrin <alexandre.vautrin21560@gmail.com>
2024-10-17 09:01:12 +0200

    Add TODOs

--- API.py
+++ API.py
@@ -5,8 +5,8 @@ app = FastAPI()
 
 @app.get("/temperature")
 async def root():
-    return {"message": "Temperature value : 20"}
+    return {"message": "Temperature value : 20"}        #TODO: change to actual value
 
 @app.get("/occupancy")
 async def root():
-    return {"message": "Occupancy value : 1"}
+    return {"message": "Occupancy value : 1"}           #TODO: change to actual value
